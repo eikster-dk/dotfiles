dab2a90d104768f4392f49cc3efeec7991fd926c
Eik Emil Bengtsson <git.noreply@e16n.dev>
2026-05-26 15:18:18 +0200

    chore: Harpoon key mapping changes

---
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -0,0 +1,20 @@
+return {
+  "ThePrimeagen/harpoon",
+  keys = {
+    {
+      "<leader>h",
+      function()
+        require("harpoon"):list():add()
+      end,
+      desc = "Harpoon File",
+    },
+    {
+      "<leader>H",
+      function()
+        local harpoon = require("harpoon")
+        harpoon.ui:toggle_quick_menu(harpoon:list())
+      end,
+      desc = "Harpoon Quick Menu",
+    },
+  },
+}
